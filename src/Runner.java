073d75e589cb62968263827762224ae4b066ec0b
andrewn <andrewnitu@gmail.com>
2017-09-29 16:53:09 -0400

    Add 1.1 tests to runner

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,11 @@
 public class Runner {
+    public static void main(String[] args) {
+        Question1_1 question1_1 = new Question1_1();
+
+        if (!((!question1_1.isUniqueChars("thelazybrowndogjumpedover"))
+                &&
+                question1_1.isUniqueChars("abcdefghijklmnopqrstuvwxyz"))) {
+            System.exit(1);
+        }
+    }
 }
