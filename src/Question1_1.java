d12eeee885cdbc054b5a7f4ef68ccfbc66f75e2d
andrewn <andrewnitu@gmail.com>
2017-09-29 16:52:53 -0400

    Add implementation for 1.1

---
+++ src/Question1_1.java
@@ -0,0 +1,29 @@
+public class Question1_1 {
+    public Question1_1() {
+    }
+
+    // O(n) time complexity.
+    // O(1) memory.
+
+    // Can also sort and then compare adjacent characters for O(nlogn) time complexity.
+    public boolean isUniqueChars(String string) {
+        boolean[] charsExist = new boolean[128];
+
+        for (int i = 0; i < 26; i++) {
+            charsExist[i] = false;
+        }
+
+        char current;
+
+        for (int i = 0; i < string.length(); i++) {
+            current = string.charAt(i);
+
+            if (charsExist[current]) {
+                return false;
+            }
+            charsExist[current] = true;
+        }
+
+        return true;
+    }
+}
