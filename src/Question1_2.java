32f8f5f1b2eb2c76c33317b2b5cca1b671fe2f7c
andrewn <andrewnitu@gmail.com>
2017-09-29 20:35:26 -0400

    Add implementation for 1.2

---
+++ src/Question1_2.java
@@ -0,0 +1,62 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Question1_2 {
+    public boolean isPermutationArray(String string1, String string2) {
+        int[] charsExist = new int[128];
+
+        // Initialize the array to be all false
+        for (int i = 0; i < 128; i++) {
+            charsExist[i] = 0;
+        }
+
+        char currentChar;
+
+        // Count every character found in the first string
+        for (int i = 0; i < string1.length(); i++) {
+            currentChar = string1.charAt(i);
+
+            charsExist[i]++;
+        }
+
+        // Count every character found in the second string
+        for (int j = 0; j < string2.length(); j++) {
+            currentChar = string2.charAt(j);
+
+            // Return false if we run out of characters in the first string found in the second string
+            if (charsExist[j] == 0) {
+                return false;
+            }
+            charsExist[j]--;
+        }
+
+        // Check that we have no leftover characters from the first string!
+        for (int i = 0; i < 128; i++) {
+            if (charsExist[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPermutationSort(String string1, String string2) {
+        char chars1[] = string1.toCharArray();
+        char chars2[] = string2.toCharArray();
+
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+
+        if (chars1.length != chars2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] != chars2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
