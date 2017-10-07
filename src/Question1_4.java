b43cdc8a0e7dc4a17c81b8fe501be94f64128997
andrewn <andrewnitu@gmail.com>
2017-10-06 20:41:22 -0400

    Add 1.4 skeleton

---
+++ src/Question1_4.java
@@ -0,0 +1,10 @@
+public class Question1_4 {
+
+    public boolean isPermutationOfPalindrome(String string) {
+        int[] charsFound = new int[128];
+
+        for (int i = 0; i < string.length(); i++) {
+            
+        }
+    }
+}
