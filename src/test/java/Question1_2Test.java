fb3f350808e9b9e49414e844aa0de19181a05728
Andrew Nitu <andrewnitu@gmail.com>
2018-02-04 02:49:38 -0500

    TravisCI & Maven setup (#1)
    * Realign project structure for Maven
    
    * Add travis yaml
    
    * Add Maven build/test instructions
    
    * Add test folder and few tests
    
    * Update gitignore to ignore Maven build artifacts
    
    * Add question 1.3 and 1.4 tests
    
    * Fix test syntax errors

---
+++ src/test/java/Question1_2Test.java
@@ -0,0 +1,20 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class Question1_2Test {
+	@Test
+	public void testArrayImpl() {
+        Question1_2 question1_2 = new Question1_2();
+
+		Assert.assertTrue(question1_2.isPermutationArray("Rotor", "toroR"));
+        Assert.assertTrue(!question1_2.isPermutationArray("Lololololol", "Lolololololo"));
+	}
+
+    @Test
+    public void testSortImpl() {
+        Question1_2 question1_2 = new Question1_2();
+
+        Assert.assertTrue(question1_2.isPermutationSort("Rotor", "toroR"));
+        Assert.assertTrue(!question1_2.isPermutationSort("Lololololol", "Lolololololo"));
+    }
+}
---
+++ src/test/java/Question1_3Test.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class Question1_3Test {
+	@Test
+	public void testUrlify() {
+        Question1_3 question1_3 = new Question1_3();
+
+        Assert.assertTrue(question1_3.urlify("Mr John Smith    ", 13).equals("Mr%20John%20Smith"));
+        Assert.assertTrue(question1_3.urlify("This is a test      ", 14).equals("This%20is%20a%20test"));
+    }
+}
