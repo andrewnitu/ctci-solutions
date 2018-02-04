b3588c565b71db30198afcac186721104f9c2db7
Andrew Nitu <andrewnitu@gmail.com>
2018-02-04 02:38:45 -0500

    Add question 1.3 and 1.4 tests

---
+++ src/test/java/Question1_4Test.java
@@ -0,0 +1,10 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class Question1_4Test {
+	@Test
+	public void testUrlify() {
+        Question1_4 question1_4 = new Question1_4();
+
+        Assert.assertTrue(question1_4.isPermutationOfPalindrome("Tact Coa")));
+}
