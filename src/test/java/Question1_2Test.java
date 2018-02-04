096ad5c3a4378d0ee41ef3092bd3b38db8ad55b1
Andrew Nitu <andrewnitu@gmail.com>
2018-02-04 14:03:31 -0500

    Improve 1.2 code coverage

--- src/test/java/Question1_2Test.java
+++ src/test/java/Question1_2Test.java
@@ -8,6 +8,7 @@ public class Question1_2Test {
 
 		Assert.assertTrue(question1_2.isPermutationArray("Rotor", "toroR"));
         Assert.assertTrue(!question1_2.isPermutationArray("Lololololol", "Lolololololo"));
+        Assert.assertTrue(!question1_2.isPermutationArray("Rotor", "Rotoe"));
 	}
 
     @Test
@@ -16,5 +17,6 @@ public class Question1_2Test {
 
         Assert.assertTrue(question1_2.isPermutationSort("Rotor", "toroR"));
         Assert.assertTrue(!question1_2.isPermutationSort("Lololololol", "Lolololololo"));
+        Assert.assertTrue(!question1_2.isPermutationArray("Rotor", "Rotoe"));
     }
 }
