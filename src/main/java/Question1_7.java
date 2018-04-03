3dc425fac2c5bb56928d6dba4423fc25707107b4
Andrew Nitu <andrewnitu@gmail.com>
2018-04-02 22:39:05 -0400

    Add question 1.7

---
+++ src/main/java/Question1_7.java
@@ -0,0 +1,25 @@
+public class Question1_7 {
+    // O(x) time complexity
+    // O(x) space complexity
+
+    // Should rotate clockwise 90 degrees
+    public void rotateMatrix(int[][] matrix) {
+        int s = matrix.length;
+
+        // Iterate each layer
+        for(int i = 0; i < s/2; i++) {
+            // Iterate each position on the side
+            for(int j = i; j < s - i - 1; j++) {
+                int tmp = matrix[i][j];
+
+                matrix[i][j] = matrix[s-j-1][i];
+
+                matrix[s-j-1][i] = matrix[s-i-1][s-j-1];
+
+                matrix[s-i-1][s-i-j] = matrix[j][s-i-1];
+
+                matrix[j][s-i-1] = tmp;
+            }
+        }
+    }
+}
